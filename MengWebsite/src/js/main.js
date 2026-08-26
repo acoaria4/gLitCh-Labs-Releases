884a29fb36ff2405dd70d56373803fbce1bb80cc
acoaria4 <diwakar.logins@gmail.com>
2026-08-26 15:43:35 +0530

    Stop tracking local website experiment folders.

--- MengWebsite/src/js/main.js
+++
@@ -1,8 +0,0 @@
-import "../styles/main.css";
-import { initMotion } from "./motion.js";
-import { initNav } from "./nav.js";
-import { initContactForm } from "./contact.js";
-
-initNav();
-initContactForm();
-initMotion();
